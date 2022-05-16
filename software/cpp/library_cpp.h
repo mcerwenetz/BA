a992d2047617bfae27245f63ac1e52f298def411
Marius Cerwenetz <cerwenetz.marius@gmail.com>
2022-05-16 15:54:23 +0200

    lib updated with enums
    this does not work with ctypes as it only works with shared libs
    which only work with gcc
    which does not work with c++ includes

--- software/cpp/library_cpp.h
+++ software/cpp/library_cpp.h
@@ -1,8 +1,31 @@
 #include "udp_client.h"
 
-class Phone {
-    
-    private
+enum request_type{
+    sensor_request,
+    rpc_request
+};
 
+enum sensor_types{
+    x_accell,
+    y_accell,
+    z_accell
+};
 
-}
+enum command{
+    write_text,
+    toggle_button,
+    vibrate
+};
+
+struct sensor_request
+{
+    const char* request_type;
+    const char* sensor_type;
+};
+
+struct rpc_request
+{
+    const char* request_type;
+    const char* command;
+    const char* arg;
+};
