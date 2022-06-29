5cdc83026995afbd341be8d274d093b4d5444aa2
Marius Cerwenetz <cerwenetz.marius@gmail.com>
2022-06-29 19:16:03 +0200

    vm measuresments added

--- software/time measurements/python_parser.py
+++ software/time measurements/python_parser.py
@@ -6,7 +6,10 @@ file = open("mes.txt","r", encoding="utf-8")
 ms = []
 lines = file.readlines()
 for line in lines:
-    ms += [int(float(line)*1000)]
+    if line.startswith("real"):
+        line = line.split("\t")[1].split("m")[1].replace(",",".")[:-2]
+        
+        ms += [int(float(line)*1000)]
 
 file.close()
 
@@ -19,5 +22,7 @@ print("mittelwert: %d" % statistics.mean(ms))
 print("standardabweichung %d" % statistics.pstdev(ms))
 
 plt.boxplot(ms)
-plt.ylabel("reaktionszeit in ms")
+plt.ylabel("RTT rpc [ms]")
+frame = plt.gca()
+frame.get_xaxis().set_visible(False)
 plt.show()
